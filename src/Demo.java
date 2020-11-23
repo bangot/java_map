e19e514814242a59037f9aff870a82c9c6b5055f
bangot <dangvanbang1802@gmail.com>
2020-11-23 08:34:10 +0700

    map

---
+++ src/Demo.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Set;
+
+public class Demo {
+    public static void main(String[] args) {
+        HashMap<String,Integer> bang= new HashMap<>();
+        bang.put("a",26);
+        bang.put("b",27);
+        bang.put("c",25);
+        bang.put("d",28);
+//        Set<String>keys=
+    }
+}
